4aebc91e3ef9c642bb793bf85c7df4ad21acb051
jayson.wang <jayson.wang@cienet.com>
2025-08-22 14:54:41 +0800

    Initialize Spring Boot project with Gradle setup, including essential configuration files, application structure, and basic application code.

---
+++ src/main/java/com/jay/brevity/BrevityApplication.java
@@ -0,0 +1,13 @@
+package com.jay.brevity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrevityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrevityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jay/brevity/BrevityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.brevity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrevityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
